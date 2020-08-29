aa178f79bdd2f2c2c8b69dce0b2470d1575d1f5e
jaykim0929 <70186327+jaykim0929@users.noreply.github.com>
2020-08-29 11:02:31 +0900

    Add files via upload

--- age.py
+++ age.py
@@ -3,13 +3,15 @@ current_year = datetime.datetime.now().year
 current_month = datetime.datetime.now().month
 current_day = datetime.datetime.now().day
 
-if current_month > birth_month:      #현재월 >생일
+birth_year, birth_month, birth_day = map(int, input('생일 입력> ').split())
+
+if current_month > birth_month:      # 현재월 >생일
     age = current_year - birth_year
-elif current_month < birth_month:    #현재월<생일
+elif current_month < birth_month:    # 현재월<생일
     age =current_year - birth_year - 1
 else:                                #현재월 == 생일
-    if current_day < birth_day:      #현재일 >= 생일  
-    age = current_year - birth_year - 1
-else:
-    age = current_year - birth_year
-print(age)
+    if current_day < birth_day:         # 현재일 >= 생일  
+        age = current_year - birth_year - 1
+    else:                                    #현재일 >= 생일
+        age = current_year - birth_year
+print('만나이는 %d 입니다.' %  age)
